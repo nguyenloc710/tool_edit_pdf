efc07d3d4f8ccd40ed8208bdf28c4ee0e0678017
nguyenloc710 <loc7102010@gmail.com>
2024-03-25 10:32:19 +0700

    fix multithreading and add if page with > height

--- tool_edit_all_file_pdf_multithreading.py
+++ tool_edit_all_file_pdf_multithreading.py
@@ -36,6 +36,12 @@ def stamp(
                                                          float(content_page.mediaBox[2]) * 0.35)),
                                                  (float(content_page.mediaBox[3]) - (
                                                          float(content_page.mediaBox[3]) * 0.33)))
+                if content_page.mediaBox.width > content_page.mediaBox.height:
+                    content_page.mergeTranslatedPage(image_page,
+                                                     (float(content_page.mediaBox[2]) - (
+                                                             float(content_page.mediaBox[2]) * 0.35)),
+                                                     (float(content_page.mediaBox[3]) - (
+                                                             float(content_page.mediaBox[3]) * 0.45)))
                 writer.add_page(content_page)
                 break
             except Exception as e:
@@ -59,7 +65,7 @@ path_source_file = "C:/CFM/DE/account-statistic-service-anh-hieu/CarDoctor AutoI
 path_folder_save_file = "C:/CFM/DE/account-statistic-service-anh-hieu/CarEditPDF-TEST/"
 log_error_path = "error.txt"
 if __name__ == "__main__":
-    path_files = get_all_files_in_folder("C:/CFM/DE/account-statistic-service-anh-hieu/CarDoctor AutoISM/ACURA")
+    path_files = get_all_files_in_folder("C:/CFM/DE/account-statistic-service-anh-hieu/CarDoctor AutoISM/TOYOTA/Camry/2006-2009/2006-2009/Location&Routing/GroundPoints")
     threading_path_files = []
     count = 1
     for index, path_file in enumerate(path_files):
@@ -120,3 +126,57 @@ if __name__ == "__main__":
             count = 0
             threading_path_files = []
         count += 1
+    try:
+        t1 = threading.Thread(
+            target=stamp(threading_path_files[0], "logo.pdf",
+                         threading_path_files[0].replace(path_source_file, path_folder_save_file)), name='t1')
+        t2 = threading.Thread(
+            target=stamp(threading_path_files[1], "logo.pdf",
+                         threading_path_files[1].replace(path_source_file, path_folder_save_file)), name='t2')
+        t3 = threading.Thread(
+            target=stamp(threading_path_files[2], "logo.pdf",
+                         threading_path_files[2].replace(path_source_file, path_folder_save_file)), name='t3')
+        t4 = threading.Thread(
+            target=stamp(threading_path_files[3], "logo.pdf",
+                         threading_path_files[3].replace(path_source_file, path_folder_save_file)), name='t4')
+        t5 = threading.Thread(
+            target=stamp(threading_path_files[4], "logo.pdf",
+                         threading_path_files[4].replace(path_source_file, path_folder_save_file)), name='t5')
+        t6 = threading.Thread(
+            target=stamp(threading_path_files[5], "logo.pdf",
+                         threading_path_files[5].replace(path_source_file, path_folder_save_file)), name='t6')
+        t7 = threading.Thread(
+            target=stamp(threading_path_files[6], "logo.pdf",
+                         threading_path_files[6].replace(path_source_file, path_folder_save_file)), name='t7')
+        t8 = threading.Thread(
+            target=stamp(threading_path_files[7], "logo.pdf",
+                         threading_path_files[7].replace(path_source_file, path_folder_save_file)), name='t8')
+        t9 = threading.Thread(
+            target=stamp(threading_path_files[8], "logo.pdf",
+                         threading_path_files[8].replace(path_source_file, path_folder_save_file)), name='t9')
+        t10 = threading.Thread(
+            target=stamp(threading_path_files[8], "logo.pdf",
+                         threading_path_files[8].replace(path_source_file, path_folder_save_file)), name='t10')
+        t1.start()
+        t2.start()
+        t3.start()
+        t4.start()
+        t5.start()
+        t6.start()
+        t7.start()
+        t8.start()
+        t9.start()
+        t10.start()
+
+        t1.join()
+        t2.join()
+        t3.join()
+        t4.join()
+        t5.join()
+        t6.join()
+        t7.join()
+        t8.join()
+        t9.join()
+        t10.join()
+    except Exception as e:
+        print("Done")
